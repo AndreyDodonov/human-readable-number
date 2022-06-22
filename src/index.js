31d0c77b03c77df3a192075c4290c9858c48c1d6
Andrew Dodonov <nordhummer@gmail.com>
2022-06-22 12:37:17 +0300

    init commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    let digits1 = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let digits2 = [ 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let digits3 = ['dummy1', 'dummy2', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+
+    // 0-9
+    if (number.toString().length == 1) {
+        return digits1[number]
+    }
+
+    // 10-19
+    if (number.toString().length == 2 && number < 20) {
+        return digits2[number - 10]
+    }
+
+    // 20-99 (include rounds digits)
+    if (number.toString().length == 2 && number >= 20) {
+        let rank10s = number.toString()[0];
+        let rank1s = number.toString()[1];
+        if (rank1s != 0) {
+            return digits3[rank10s] + ' ' + digits1[rank1s];
+        }
+        return digits3[rank10s];
+    }
+
+    // 100-999 (include rounds digits)
+    if (number.toString().length == 3) {
+        let rank100s = number.toString()[0];
+        let rank10s = number.toString()[1];
+        let rank1s = number.toString()[2];
+
+        if (rank1s == 0 && rank10s == 0) {    //200-900 rounds        
+            return digits1[rank100s] + ' hundred'; // 100 & 200-900 (rounds)
+        } else if (rank10s == 0) {
+            return   digits1[rank100s] + ' hundred ' + digits1[rank1s]; // x01-x09
+        } else if (rank1s == 0 && rank10s != 1 ) { // xx0
+            return  digits1[rank100s] + ' hundred ' + digits3[rank10s];
+        } else if(rank10s == 1) {
+            return digits1[rank100s] + ' hundred ' + digits2[rank1s];
+        }
+        return digits1[rank100s] + ' hundred ' + digits3[rank10s] + ' ' + digits1[rank1s];
+
+    }  
 }
+
